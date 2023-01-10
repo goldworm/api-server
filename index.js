0fe78ae37605711f3857c7b2410a584c52375b6f
Chiwon Cho <superdoli@gmail.com>
2023-01-10 23:25:24 +0900

    Add /user/id route

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
-const host = '0.0.0.0'
-const port = 8080
+import express from 'express';
+import { getUserId } from './utils.js';
+
+const app = express();
+const host = '0.0.0.0';
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
+
+app.get('/user/:id', (req, res) => {
+  res.send(getUserId(req.params.id));
+});
 
 app.listen(port, host, () => {
-  console.log(`Example app listening on ${host}:${port}`)
-})
+  console.log(`Example app listening on ${host}:${port}`);
+});
